b8040450a0cfbd876b70ce752b2f0371a27c5513
Jaison PR <jaisonpr@gmail.com>
2020-11-10 14:53:06 -0300

    Express Serve Static Files

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -5,8 +5,12 @@ const
 const hostname = 'localhost';
 const port = 3000;
 
+const morgan = require('morgan');
+
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
