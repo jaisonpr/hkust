6bc22a97ab1edd1d64530927428eae43c49a2950
Jaison PR <jaisonpr@gmail.com>
2020-11-16 16:49:10 -0300

    Passport Facebook

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '697959544255584',
+        clientSecret: '9dfa4b3e85e418a36b05ae7e337a4da8'
+    }
 }
