127cb1ca14d6694bcb2c98a7b02c9b4f515ba8e0
Jaison PR <jaisonpr@gmail.com>
2020-11-16 10:16:39 -0300

    File Upload

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -9,6 +9,7 @@ var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
+const uploadRouter = require('./routes/uploadRouter');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRouter');
 var dishRouter = require('./routes/dishRouter');
@@ -66,14 +67,13 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/imageUpload',uploadRouter);
 
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leadersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
