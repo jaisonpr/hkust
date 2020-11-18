2d56c0c1def5c7268d4e4bde146f0c18542166f2
Jaison PR <jaisonpr@gmail.com>
2020-11-18 15:27:44 -0300

    Assignment 1

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -6,6 +6,8 @@ import { Icon } from 'react-native-elements';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 
 const HomeNavigator = createStackNavigator({
     Home: { screen: Home }
@@ -39,6 +41,40 @@ const MenuNavigator = createStackNavigator({
     }
 );
 
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About }
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact }
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+
 const MainNavigator = createDrawerNavigator({
     Home: 
       { screen: HomeNavigator,
@@ -47,12 +83,26 @@ const MainNavigator = createDrawerNavigator({
           drawerLabel: 'Home'
         }
       },
+    About: {
+        screen: AboutNavigator,
+        navigationOptions: {
+          title: "About Us",
+          drawerLabel: "About Us"
+        }
+      },
     Menu: 
       { screen: MenuNavigator,
         navigationOptions: {
           title: 'Menu',
           drawerLabel: 'Menu'
         }, 
+      },
+    Contact: {
+        screen: ContactNavigator,
+        navigationOptions: {
+          title: "Contact Us",
+          drawerLabel: "Contact Us"
+        }
       }
 }, {
   drawerBackgroundColor: '#D1C4E9'
