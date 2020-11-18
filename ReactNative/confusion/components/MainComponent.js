444e8be8913db135d1bdcf2a27c73016ce77d161
Jaison PR <jaisonpr@gmail.com>
2020-11-18 09:56:23 -0300

    Navigation Part 1

---
+++ ReactNative/confusion/components/MainComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+        Menu: { screen: Menu },
+        Dishdetail: { screen: Dishdetail }
+    },
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }
+    }
+);
+
+class Main extends Component {
+
+    render() {
+ 
+    return (
+        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+            <MenuNavigator />
+        </View>
+    );
+  }
+}
+  
+export default Main;
