b783d7910ef852037800fe5ef1d45718340c50b6
Jaison PR <jaisonpr@gmail.com>
2020-11-22 13:25:33 -0300

    Persist Redux Store

--- ReactNative/confusion/redux/configureStore.js
+++ ReactNative/confusion/redux/configureStore.js
@@ -1,15 +1,25 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
 import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
 
+
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+  }
+
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             dishes,
             comments,
             promotions,
@@ -19,5 +29,7 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store)
+
+    return { persistor, store };
 }
