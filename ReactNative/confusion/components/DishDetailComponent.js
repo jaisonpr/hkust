a8435f35b8113e838770130673dd48f42e3ff464
Jaison PR <jaisonpr@gmail.com>
2020-11-22 12:50:03 -0300

    Gestures Part 1

--- ReactNative/confusion/components/DishDetailComponent.js
+++ ReactNative/confusion/components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, FlatList, StyleSheet, Modal, Button } from "react-native";
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from 'react-redux';
 import * as Animatable from 'react-native-animatable';
@@ -54,11 +54,40 @@ function RenderComments(props) {
 function RenderDish(props) {
 
     const dish = props.dish;
+
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+      if ( dx < -200 )
+          return true;
+      else
+          return false;
+    }
     
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gestureState) => {
+          return true;
+      },
+      onPanResponderEnd: (e, gestureState) => {
+          console.log("pan responder end", gestureState);
+          if (recognizeDrag(gestureState))
+              Alert.alert(
+                  'Add Favorite',
+                  'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                  [
+                  {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                  {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
+                  ],
+                  { cancelable: false }
+              );
+
+          return true;
+      }
+    })
+
     if (dish != null) {
         return(
 
-          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+            {...panResponder.panHandlers}>
             <Card featuredTitle={dish.name} image={{uri: baseUrl + dish.image}}>
                 <Text style={{margin: 10}}> {dish.description} </Text>                
                 <View style={{ flexDirection: "row", justifyContent: "center" }}>
