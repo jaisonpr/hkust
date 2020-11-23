598627c1f44053fb9f9e3b0f5b96e64ab06a3038
Jaison PR <jaisonpr@gmail.com>
2020-11-23 11:33:16 -0300

    Social Sharing

--- ReactNative/confusion/components/DishDetailComponent.js
+++ ReactNative/confusion/components/DishDetailComponent.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import React, { Component } from 'react';import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from 'react-redux';
 import * as Animatable from 'react-native-animatable';
@@ -100,6 +99,16 @@ function RenderDish(props) {
       }
     })
 
+    const shareDish = (title, message, url) => {
+      Share.share({
+          title: title,
+          message: title + ': ' + message + ' ' + url,
+          url: url
+      },{
+          dialogTitle: 'Share ' + title
+      })
+    }
+
     if (dish != null) {
         return(
 
@@ -127,6 +136,14 @@ function RenderDish(props) {
                     color="#512DA8"
                     onPress={() => props.toggleModal()}
                 />
+                <Icon
+                    raised
+                    reverse
+                    name='share'
+                    type='font-awesome'
+                    color='#51D2A8'
+                    style={styles.cardItem}
+                    onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                 </View>
             </Card>
           </Animatable.View>
